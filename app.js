5ad112cf3a65acdd1d42b2e5829e1d57a03d582b
Michael Dao <michaeltuandao@gmail.com>
2019-06-03 13:31:35 -0500

    successful setup of index

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+let express = require('express');
+let app = express();
+let port = 3000;
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(require('./routes/index'));
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res)=>{
-    res.render('Index')
+    res.send('index')
 })
 
 module.exports = router;
