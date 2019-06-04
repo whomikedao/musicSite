00be8f9df86e8bb766e6d4c4aa7b3f9cb9d7dcb1
Michael Dao <michaeltuandao@gmail.com>
2019-06-04 02:43:17 -0500

    updated index js to find tenD

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../data/data.json');
 
 router.get('/', (req, res)=>{
-    res.send('index')
+    let albumList = []
+    let description = data.TenD;
+
+    description.forEach((albumObj)=>{
+        albumList = albumList.concat(albumObj.album);
+    })
+
+    console.log(albumList);
+    res.send('TenD')
 })
 
 module.exports = router;
